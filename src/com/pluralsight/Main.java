5ea6db40f3bdf79cd00bc683a283e14bda79f1ef
Kiki <kiki_e@hotmail.com>
2017-12-17 14:45:05 -0500

    Initial commit

---
+++ src/com/pluralsight/Main.java
@@ -0,0 +1,38 @@
+package com.pluralsight;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception{
+	// write your code here
+        int repeats = 5;
+        int steps_per_second = 5;
+
+        for ( int i=0; i<repeats*11 ; i++ )
+        {
+            if ( i%11 == 0 )
+                System.out.print(" .oOo..... \r");
+            else if ( i%11 == 1 )
+                System.out.print(" ..oOo.... \r");
+            else if ( i%11 == 2 )
+                System.out.print(" ...oOo... \r");
+            else if ( i%11 == 3 )
+                System.out.print(" ....oOo.. \r");
+            else if ( i%11 == 4 )
+                System.out.print(" .....oOo. \r");
+            else if ( i%11 == 5 )
+                System.out.print(" ......oOo \r");
+            else if ( i%11 == 6 )
+                System.out.print(" .......oO \r");
+            else if ( i%11 == 7 )
+                System.out.print(" o.......o \r");
+            else if ( i%11 == 8 )
+                System.out.print(" Oo....... \r");
+            else if ( i%11 == 9 )
+                System.out.print(" oOo...... \r");
+            else if ( i%11 == 10 )
+                System.out.print(" .oOo..... \r");
+
+            Thread.sleep(1000/steps_per_second);
+        }
+    }
+}
